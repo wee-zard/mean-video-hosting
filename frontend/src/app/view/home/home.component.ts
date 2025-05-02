bb419585ccc001e9233a63532ee106aa2f09625e
weezard <veelmoth@gmail.com>
2025-05-02 17:55:36 +0200

    Creating the home component
    Implementing the home component, where the filtered videos will be displayed to the users when they visit this page.

--- frontend/src/app/view/home/home.component.ts
+++ frontend/src/app/view/home/home.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { VideoService } from '../../shared/services/video.service';
+import { VideoSearchRequest } from '../../shared/models/VideoSearchRequest';
+import { SnackbarService } from '../../shared/services/snackbar.service';
+import { SnackbarSeverityEnums } from '../../shared/enums/SnackbarSeverityEnums';
+import { CommonModule } from '@angular/common';
+import { VideoResponse } from '../../shared/models/response/VideoResponse';
+import { VideoCardComponent } from '../../component/video-card/video-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, VideoCardComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  listOfVideos: VideoResponse[] = [];
+  videoSearchRequest: VideoSearchRequest = {};
 
+  constructor(
+    private videoService: VideoService,
+    private snackbarService: SnackbarService,
+  ) {}
+
+  ngOnInit(): void {
+    this.videoService
+      .getVideosBySearchParams(this.videoSearchRequest)
+      .then((videos) => {
+        videos = [videos[0]];
+        this.listOfVideos = [...videos];
+      })
+      .catch((error) =>
+        this.snackbarService.open(
+          SnackbarSeverityEnums.ERROR,
+          error?.error?.error ?? error.message,
+        ),
+      );
+  }
 }
